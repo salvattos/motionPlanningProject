3ec6b57bf29c55d343d3fc0c4cf4222650796f0f
kitcat425.314 <kitcat425.314@gmail.com>
2023-04-26 17:15:01 -0400

    got basic attract to plot in 2D without some tuning parameters

--- finalProject.py
+++ finalProject.py
@@ -1,13 +1,28 @@
 import numpy as np
 from APFObstacle import APFObstacle
+#from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+
+
 
 def calcFatt(xi,pG,KV,VG,V):
-    Fatt = -xi*pG + (KV*(VG-V))
+    #Fatt = np.dot(-xi,pG) + (KV*(VG-V))
+    u = -xi * pG[0]
+    v = -xi * pG[1]
+    Fatt = [u,v]
     return Fatt
 
 
 def calcpG(goalPos,currentPos):
-    pG = currentPos-goalPos #might need to switch order
+    print(currentPos[0])
+    print(currentPos[1])
+    print(np.shape(currentPos))
+    print(goalPos)
+    print(np.shape(goalPos))
+    #pG = np.subtract(currentPos,goalPos) #might need to switch order
+    u = currentPos[0] - goalPos[0]
+    v = currentPos[1] - goalPos[1]
+    pG = [u,v]
     return pG
 
 
@@ -25,15 +40,64 @@ KV = 60
 OB1 = APFObstacle(obstaclePos,goalPos,p0,V0)
 
 Frep = OB1.calcFrepTotal(obstaclePos+np.array([3,4]),V)
-FattList = []
-for x in range(0,100):
-    for y in range(0,100):
-        currentPos = np.array([x,y])
-        #Attraction force
-        Fatt = calcFatt(xi,calcpG(goalPos,currentPos),KV,VG,V)
-        FattList.append(Fatt)
-        #Frep = OB1.calcFrepTotal(currentPos,V)
-        Frep = 0
-        Fsum = Frep+Fatt
+#FattList = []
+uatt = []
+vatt = []
 
-print(FattList)
+x, y = np.meshgrid(np.arange(0, 100, 10),
+                   np.arange(0, 100, 10))
+
+#u,v = calcFatt(xi,calcpG(goalPos,np.array([x,y])),KV,VG,V)
+
+u = []
+v = []
+
+#for x in np.arange(0,100,10):
+#    for y in np.arange(0,100,10):
+#        currentPos = np.array([x,y])
+#        u.append(calcFatt(xi,calcpG(goalPos,currentPos),KV,VG,V)[0])
+#        v.append(calcFatt(xi,calcpG(goalPos,currentPos),KV,VG,V)[1])
+#        currentPos = np.array([x,y])
+#        uatt.append(calcFatt(xi,calcpG(goalPos,currentPos),KV,VG,V)[0])
+#        vatt.append(calcFatt(xi,calcpG(goalPos,currentPos),KV,VG,V)[1])
+        #FattList.append(Fatt)
+        #Frep = 0
+        #Fsum = np.add(Frep, Fatt)
+#u = np.array(u)
+#v = np.array(v)
+#u = 
+
+u = calcFatt(xi,calcpG(goalPos,[x,y]),KV,VG,V)[0]
+v = calcFatt(xi,calcpG(goalPos,[x,y]),KV,VG,V)[1]
+#u = -(y-x)
+#v = -(-y-x)
+w = 0
+#x, y, z = np.meshgrid(np.arange(-0.8, 1, 0.3),
+#                      np.arange(-0.8, 1, 0.3),
+#                      np.arange(-0.8, 1, 0.3))
+
+#u = -(y-x)#np.sin(np.pi * x) * np.cos(np.pi * y) * np.cos(np.pi * z)
+#v = -(-y-x)#-np.cos(np.pi * x) * np.sin(np.pi * y) * np.cos(np.pi * z)
+#w = -(-z-x)#(np.sqrt(2.0 / 3.0) * np.cos(np.pi * x) * np.cos(np.pi * y) * np.sin(np.pi * z))
+#z=0
+#print(z)
+
+#print(x)
+#print(y)
+#for x in range(0,100):
+#    for y in range(0,100):
+#        currentPos = np.array([x,y])
+        #Attraction force
+#        Fatt = calcFatt(xi,calcpG(goalPos,currentPos),KV,VG,V)
+#        FattList.append(Fatt)
+        #Frep = OB1.calcFrepTotal(currentPos,V)
+#        Frep = 0
+#        Fsum = Frep+Fatt
+#u, v = 
+#fig = plt.figure()
+#ax = fig.add_subplot(projection="3d")
+plt.quiver(x, y, u, v)
+plt.grid()
+plt.show()
+
+#print(FattList)

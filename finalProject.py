97f9092c7e14aba48025add97e8ff900824b2195
kitcat425.314 <kitcat425.314@gmail.com>
2023-04-27 06:53:21 -0400

    created repulsion function. Dropoff is tunable. Dropoff radius is not working yet...

--- finalProject.py
+++ finalProject.py
@@ -2,7 +2,30 @@ import numpy as np
 from APFObstacle import APFObstacle
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
+import math
 
+obstacles = np.array([50,50])
+goalPos  = np.array([60,60])
+
+startPos = np.array([10,15])
+xi = .100
+VG = np.array([0,0])
+V0 = np.array([0,0])
+V = np.array([0,0])
+p0 = 5.
+KV = 60.
+
+#goalPos = goalPos
+#V0 = V0
+#p0 = p0
+#obstaclePos = obstaclePos   
+nu = 80.
+n = 2.
+#KV = 60
+KVPrime = 70.
+
+k=0.03
+i=100000000000000
 
 
 def calcFatt(xi,pG,KV,VG,V):
@@ -14,61 +37,67 @@ def calcFatt(xi,pG,KV,VG,V):
 
 
 def calcpG(goalPos,currentPos):
-    #print(currentPos[0])
-    #print(currentPos[1])
-    #print(np.shape(currentPos))
-    #print(goalPos)
     #print(np.shape(goalPos))
-    #pG = np.subtract(currentPos,goalPos) #might need to switch order
+    #print(np.shape(currentPos))
     u = currentPos[0] - goalPos[0]
     v = currentPos[1] - goalPos[1]
     pG = [u,v]
     return pG
 
+def magnitude(vector):
+    return np.sqrt(sum(pow(element, 2) for element in vector))
 
-obstaclePos = np.array([50,50])
-goalPos  = np.array([95,85])
+def calcFrep(obsPos, currentPos):
+    #print(np.shape(obsPos))
+    #print(obsPos)
+    pQ = calcpG(obsPos, currentPos)
+    #Frep = self.nu*(pQ**-1 - self.p0**-1)*((pQ**-1)**2)*np.power((currentPos-self.goalPos),self.n)
+    
+    #building my own repulsion because this one sucks
+    u = -(y-x)#/np.sqrt(sum(pow(-(y-x),2), pow(-(-y-x),2)))
+    v = -(-y-x)#/np.sqrt(sum(pow(-(y-x),2), pow(-(-y-x),2)))
+    
+    #u = nu*((1/pQ[0])-(1/p0)*((1/pQ[0])**2))#*(currentPos[0]-obsPos[0]))
+    #v = nu*((1/pQ[1])-(1/p0)*((1/pQ[1])**2))#*(currentPos[1]-obsPos[1]))
+    
+    #u = nu*(np.power(pQ[0],-1) - np.power(float(p0),-1))*(np.power(float(pQ[0]),-1)**2)#*np.power((currentPos[0] - goalPos[0]),n))
+    #v = nu*(pQ[1]**-1 - p0**-1)*((pQ[1]**-1**2))#*np.power((currentPos[1] - goalPos[1]),n))
+    Frep = [u,v]#/(magnitude([u,v])))#*(1/(1 + np.exp(k*(magnitude([u,v])-i))))
+    print("before Scaleup")
+    print(Frep)
+    #Frep = np.multiply(Frep, 200)
+    #Frep = Frep * 200
+    Frep = Frep/(magnitude([u,v]))*1/(1+np.exp(k*(magnitude([u,v])-i)))
+    #Frep = (100*Frep).astype(int)
+    print("after Scaleup")
+    print(Frep)
+    #Frep = Frep * np.full(np.shape(Frep), 1)
+    #print("after Scaleup 2")
+    #print(Frep)
+    return Frep
 
-startPos = np.array([10,15])
-xi = .100
-VG = np.array([0,0])
-V0 = np.array([0,0])
-V = np.array([0,0])
-p0 = 5
-KV = 60
-
-OB1 = APFObstacle(obstaclePos,goalPos,p0,V0)
-
-Frep = OB1.calcFrepTotal(obstaclePos+np.array([3,4]),V)
-#FattList = []
 uatt = []
 vatt = []
 
-x, y = np.meshgrid(np.arange(0, 100, 10),
-                   np.arange(0, 100, 10))
+x, y = np.meshgrid(np.arange(0, 100, 10, dtype=float),
+                   np.arange(0, 100, 10, dtype=float))
 
-#u,v = calcFatt(xi,calcpG(goalPos,np.array([x,y])),KV,VG,V)
+uatt = calcFatt(xi,calcpG(goalPos,[x,y]),KV,VG,V)[0]
+vatt = calcFatt(xi,calcpG(goalPos,[x,y]),KV,VG,V)[1]
+Fatt = [uatt, vatt]
 
-u = []
-v = []
 
-#for x in np.arange(0,100,10):
-#    for y in np.arange(0,100,10):
-#        currentPos = np.array([x,y])
-#        u.append(calcFatt(xi,calcpG(goalPos,currentPos),KV,VG,V)[0])
-#        v.append(calcFatt(xi,calcpG(goalPos,currentPos),KV,VG,V)[1])
-#        currentPos = np.array([x,y])
-#        uatt.append(calcFatt(xi,calcpG(goalPos,currentPos),KV,VG,V)[0])
-#        vatt.append(calcFatt(xi,calcpG(goalPos,currentPos),KV,VG,V)[1])
-        #FattList.append(Fatt)
-        #Frep = 0
-        #Fsum = np.add(Frep, Fatt)
-#u = np.array(u)
-#v = np.array(v)
-#u = 
 
-u = calcFatt(xi,calcpG(goalPos,[x,y]),KV,VG,V)[0]
-v = calcFatt(xi,calcpG(goalPos,[x,y]),KV,VG,V)[1]
+F = np.zeros((10,10))#Fatt
+
+for i,obsPos in enumerate(obstacles):
+    urep = calcFrep([10,20], [x,y])[0]
+    vrep = calcFrep([10,20], [x,y])[1]
+    Frep = [urep, vrep]
+    F = np.add(F, Frep)
+print(type(F))
+print(np.shape(F))
+print(F)
 #u = -(y-x)
 #v = -(-y-x)
 w = 0
@@ -96,7 +125,7 @@ w = 0
 #u, v = 
 #fig = plt.figure()
 #ax = fig.add_subplot(projection="3d")
-plt.quiver(x, y, u, v)
+plt.quiver(x, y, F[0], F[1])
 plt.grid()
 plt.show()
 
